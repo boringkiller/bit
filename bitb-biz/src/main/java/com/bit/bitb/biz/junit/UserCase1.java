7fc555a46a6d914681da673e6deb65bb15960aed
jidu <jdu@hp.com>
2013-04-29 22:52:49 +0800

    user case 1 with mockito

--- bitb-biz/src/main/java/com/bit/bitb/biz/junit/UserCase1.java
+++ bitb-biz/src/main/java/com/bit/bitb/biz/junit/UserCase1.java
@@ -13,6 +13,7 @@ import com.bit.bitb.biz.entity.Buying;
 import com.bit.bitb.biz.entity.Selling;
 import com.bit.bitb.biz.service.TradeService;
 import com.bit.bitb.biz.util.OrderUtil;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/bitb-all.xml" })
@@ -23,6 +24,8 @@ public class UserCase1 {
 
 	@Test
 	public void test() {
+		
+		tradeService = mock(TradeService.class);
 		Date now = new Date();
 		System.out.println(tradeService);
 		Selling userAselling = new Selling();
@@ -37,19 +40,35 @@ public class UserCase1 {
 		userAselling.setValidTo(validTo.getTime());
 		
 		//user A sell 1 btc price 500
-		tradeService.sell(userAselling);
+		System.out.println("user A sell 1 btc price 500 :");
+		when(tradeService.sell(userAselling)).thenReturn(false);
+		System.out.println(tradeService.sell(userAselling));
 
 		Buying userBbuying = new Buying();
 		userBbuying.setIdbuying("B" + OrderUtil.genereateOrderNumberString());
-		userBbuying.setBuyer("001");
+		userBbuying.setBuyer("002");
 		userBbuying.setPrice(499f);
 		userBbuying.setQty(1f);
 		userBbuying.setValidFrom(now);
 		userBbuying.setValidTo(validTo.getTime());
 		
 		//user B buy 1 btc price 499
-		tradeService.buy(userBbuying);
+		System.out.println("user B buy 1 btc price 499 :");
+		when(tradeService.buy(userBbuying)).thenReturn(false);
+		System.out.println(tradeService.buy(userBbuying));
 		
+		Buying userCbuying = new Buying();
+		userCbuying.setIdbuying("B" + OrderUtil.genereateOrderNumberString());
+		userCbuying.setBuyer("003");
+		userCbuying.setPrice(501f);
+		userCbuying.setQty(1f);
+		userCbuying.setValidFrom(now);
+		userCbuying.setValidTo(validTo.getTime());
+		
+		//user C buy 1 btc price 501
+		System.out.println("user C buy 1 btc price 501 :");
+		when(tradeService.buy(userCbuying)).thenReturn(true);
+		System.out.println(tradeService.buy(userCbuying));
 		
 	}
 
