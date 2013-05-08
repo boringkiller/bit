0e68990447f58d2cc6bd76c6039df489b70c1f58
dudadan <jamesdu85@qq.com>
2013-05-07 22:53:22 -0700

    userservice

---
+++ bitb-biz/src/main/java/com/bit/bitb/biz/service/UserService.java
@@ -0,0 +1,14 @@
+package com.bit.bitb.biz.service;
+
+import com.bit.bitb.biz.entity.User;
+
+public interface UserService {
+
+	public boolean increaseMoneyBalance(User user, float num);
+
+	public boolean reduceMoneyBalance(User user, float num);
+
+	public boolean increaseBtcBalance(User user, float num);
+
+	public boolean reduceBtcBalance(User user, float num);
+}
