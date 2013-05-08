824076963f2b01d4727e2b38a9165fcb267614c1
dudadan <jamesdu85@qq.com>
2013-05-07 21:26:23 -0700

    add usercase2

--- bitb-biz/src/test/java/com/bit/bitb/biz/uc/UserCase1.java
+++ bitb-biz/src/test/java/com/bit/bitb/biz/uc/UserCase1.java
@@ -31,22 +31,25 @@ public class UserCase1 {
 		tradeService = mock(TradeService.class);
 		Date now = new Date();
 		System.out.println(tradeService);
+		Calendar validTo = Calendar.getInstance();
+		validTo.setTime(now);
+		validTo.add(Calendar.MONTH, 1);
+		
+		
+		//user A sell 1 btc price 500
+		System.out.println("user A sell 1 btc price 500 :");
 		Selling userAselling = new Selling();
 		userAselling.setIdselling("S" + OrderUtil.genereateOrderNumberString());
 		userAselling.setSeller("001");
 		userAselling.setPrice(500f);
 		userAselling.setQty(1f);
 		userAselling.setValidFrom(now);
-		Calendar validTo = Calendar.getInstance();
-		validTo.setTime(now);
-		validTo.add(Calendar.MONTH, 1);
 		userAselling.setValidTo(validTo.getTime());
-		
-		//user A sell 1 btc price 500
-		System.out.println("user A sell 1 btc price 500 :");
 		when(tradeService.sell(userAselling)).thenReturn(false);
 		System.out.println(tradeService.sell(userAselling));
 
+		//user B buy 1 btc price 499
+		System.out.println("user B buy 1 btc price 499 :");
 		Buying userBbuying = new Buying();
 		userBbuying.setIdbuying("B" + OrderUtil.genereateOrderNumberString());
 		userBbuying.setBuyer("002");
@@ -54,12 +57,11 @@ public class UserCase1 {
 		userBbuying.setQty(1f);
 		userBbuying.setValidFrom(now);
 		userBbuying.setValidTo(validTo.getTime());
-		
-		//user B buy 1 btc price 499
-		System.out.println("user B buy 1 btc price 499 :");
 		when(tradeService.buy(userBbuying)).thenReturn(false);
 		System.out.println(tradeService.buy(userBbuying));
 		
+		//user C buy 1 btc price 501
+		System.out.println("user C buy 1 btc price 501 :");
 		Buying userCbuying = new Buying();
 		userCbuying.setIdbuying("B" + OrderUtil.genereateOrderNumberString());
 		userCbuying.setBuyer("003");
@@ -67,9 +69,6 @@ public class UserCase1 {
 		userCbuying.setQty(1f);
 		userCbuying.setValidFrom(now);
 		userCbuying.setValidTo(validTo.getTime());
-		
-		//user C buy 1 btc price 501
-		System.out.println("user C buy 1 btc price 501 :");
 		when(tradeService.buy(userCbuying)).thenReturn(true);
 		System.out.println(tradeService.buy(userCbuying));
 		
