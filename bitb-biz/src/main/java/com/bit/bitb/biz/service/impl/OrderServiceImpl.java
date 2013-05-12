c12b8f5fa57b2aee14e790440e895028a2f683ed
unknown <chinaboy.xfc@gmail.com>
2013-05-12 19:49:48 +0800

    修改一个报错

--- bitb-biz/src/main/java/com/bit/bitb/biz/service/impl/OrderServiceImpl.java
+++ bitb-biz/src/main/java/com/bit/bitb/biz/service/impl/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import com.bit.bitb.biz.enums.UserCapacityEnum;
 import com.bit.bitb.biz.models.BalanceModel;
 import com.bit.bitb.biz.service.BalanceService;
 import com.bit.bitb.biz.service.OrderService;
+import com.bit.bitb.biz.util.MathUtils;
 
 public class OrderServiceImpl implements OrderService {
 	private BalanceService balanceService;
@@ -11,8 +12,13 @@ public class OrderServiceImpl implements OrderService {
 	public boolean place(String userId,double price, double quantity,
 			UserCapacityEnum userCapacity) {
 		// TODO 入参校验
+		//TODO 校验可用人民币>0,==改在设置完价格和数量时校验，可用人民币大于price*quantity
+		
+		double totalRmb=MathUtils.multWithAccurated(price, quantity);
+		
 		BalanceModel b=balanceService.findBalanceByUserId(userId);
 		if (userCapacity==UserCapacityEnum.buyer) {
+			b.setUsableMoney(MathUtils.sub(b.getUsableMoney(), totalRmb));
 		}
 		return true;
 	}
