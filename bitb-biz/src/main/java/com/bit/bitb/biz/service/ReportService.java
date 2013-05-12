c9776b5ee041b89a6bd01ac24f45a7ec94505853
dudadan <jamesdu85@qq.com>
2013-05-12 07:54:50 -0700

    增加注释

--- bitb-biz/src/main/java/com/bit/bitb/biz/service/ReportService.java
+++ bitb-biz/src/main/java/com/bit/bitb/biz/service/ReportService.java
@@ -7,7 +7,17 @@ import com.bit.bitb.biz.entity.Selling;
 
 public interface ReportService {
 
+	/**
+	 * 获取购买订单前几名
+	 * @param top
+	 * @return
+	 */
 	public List<Buying> getTopBuyings(int top);
 	
+	/**
+	 * 获取出售订单前几名
+	 * @param top
+	 * @return
+	 */
 	public List<Selling> getTopSellings(int top);
 }
