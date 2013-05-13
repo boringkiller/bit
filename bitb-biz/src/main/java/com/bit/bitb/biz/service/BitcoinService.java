b2a5ca8b2fb83cc42f123acc8f63eb1731d24fe4
jidu <jdu@hp.com>
2013-05-13 21:55:11 +0800

    增加对外交互的service

---
+++ bitb-biz/src/main/java/com/bit/bitb/biz/service/BitcoinService.java
@@ -0,0 +1,10 @@
+package com.bit.bitb.biz.service;
+
+/**
+ * 负责与bitcoin客户端交互，操作比特币
+ * @author jimmy.du
+ * May 13, 2013
+ */
+public interface BitcoinService {
+
+}
---
+++ bitb-biz/src/main/java/com/bit/bitb/biz/service/MoneyService.java
@@ -0,0 +1,10 @@
+package com.bit.bitb.biz.service;
+
+/**
+ * 负责与alipay支付宝交互，操作现金
+ * @author jimmy.du
+ * May 13, 2013
+ */
+public interface MoneyService {
+
+}
