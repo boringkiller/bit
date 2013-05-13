634036fcf7fd4753b5d4f741580b2fd3441d9549
jidu <jdu@hp.com>
2013-05-13 21:36:18 +0800

    split tradeservice, add FreezeService&BalanceService

--- bitb-biz/src/main/java/com/bit/bitb/biz/service/TradeService.java
+++ bitb-biz/src/main/java/com/bit/bitb/biz/service/TradeService.java
@@ -3,7 +3,6 @@ package com.bit.bitb.biz.service;
 import com.bit.bitb.biz.entity.Buying;
 import com.bit.bitb.biz.entity.Deal;
 import com.bit.bitb.biz.entity.Selling;
-import com.bit.bitb.biz.entity.User;
 
 public interface TradeService {
 
@@ -61,38 +60,4 @@ public interface TradeService {
 	 */
 	public boolean receiveDealCost(Deal donedealAC);
 	
-	/**
-	 * 卖家出售时，冻结他要出售的那部分比特币
-	 * @param selling
-	 * @return
-	 */
-	public boolean freezeBtcWhenSell(Selling selling);
-
-	/**
-	 * 买家买入时，冻结他出价的那部分资金
-	 * @param buying
-	 * @return
-	 */
-	public boolean freezeMoneyWhenBuy(Buying buying);
-
-	/**
-	 * 解冻，在出售订单取消的时候
-	 * @param selling
-	 * @return
-	 */
-	public boolean unfreezeWhenCancel(Selling selling);
-
-	/**
-	 * 解冻，在购买订单取消的时候
-	 * @param buying
-	 * @return
-	 */
-	public boolean unfreezeWhenCancel(Buying buying);
-
-	
-	/**
-	 * 短信验证
-	 * @return
-	 */
-	public boolean doSMSverification(User user);
 }
--- bitb-biz/src/main/java/com/bit/bitb/biz/service/UserService.java
+++ bitb-biz/src/main/java/com/bit/bitb/biz/service/UserService.java
@@ -5,35 +5,9 @@ import com.bit.bitb.biz.entity.User;
 public interface UserService {
 
 	/**
-	 * 现金账户增加
-	 * @param user
-	 * @param num
+	 * 短信验证
 	 * @return
 	 */
-	public boolean increaseMoneyBalance(User user, float num);
-
-	/**
-	 * 现金账户减少
-	 * @param user
-	 * @param num
-	 * @return
-	 */
-	public boolean reduceMoneyBalance(User user, float num);
-
-	/**
-	 * 比特币账户增加
-	 * @param user
-	 * @param num
-	 * @return
-	 */
-	public boolean increaseBtcBalance(User user, float num);
-
-	/**
-	 * 比特币账户减少
-	 * @param user
-	 * @param num
-	 * @return
-	 */
-	public boolean reduceBtcBalance(User user, float num);
+	public boolean doSMSverification(User user);
 	
 }
