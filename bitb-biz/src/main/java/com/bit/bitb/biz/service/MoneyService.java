6a9c57c1cf688fe06f01d74f54d4acad2e5e6928
jidu <jdu@hp.com>
2013-05-19 20:40:14 +0800

    给Moneyservice增加参数

--- bitb-biz/src/main/java/com/bit/bitb/biz/service/MoneyService.java
+++ bitb-biz/src/main/java/com/bit/bitb/biz/service/MoneyService.java
@@ -13,29 +13,29 @@ public interface MoneyService {
 	 * 充值确认请求,由客户发起
 	 * @return
 	 */
-	public boolean depositConfirmRequest(User user, double money);
+	public boolean depositConfirmRequest(User customer, double money);
 
 	/**
 	 * 充值确认，由客服完成
 	 * @return
 	 */
-	public boolean depositConfirm(User user, double money);
+	public boolean depositConfirm(User customer, double money, boolean isDeposited, User customerService);
 	
 	/**
 	 * 提现请求，由客户发起
 	 * @return
 	 */
-	public boolean withdrawDepositRequest(User user, double money);
+	public boolean withdrawDepositRequest(User customer, double money);
 	
 	/**
 	 * 客服打钱到客户支付宝后，请求客户确认
 	 * @return
 	 */
-	public boolean withdrawDepositConfirmRequest(User user, double money);
+	public boolean withdrawDepositConfirmRequest(User customer, double money, User customerService);
 	
 	/**
 	 * 客户提现之后确认
 	 * @return
 	 */
-	public boolean withdrawDepositConfirm(User user, double money);
+	public boolean withdrawDepositConfirm(User customer, double money, boolean isWithdrawed);
 }
--- bitb-biz/src/main/java/com/bit/bitb/biz/service/impl/MoneyServiceImpl.java
+++ bitb-biz/src/main/java/com/bit/bitb/biz/service/impl/MoneyServiceImpl.java
@@ -1,41 +1,43 @@
 package com.bit.bitb.biz.service.impl;
 
 import com.bit.bitb.biz.entity.User;
-import com.bit.bitb.biz.service.BalanceService;
 import com.bit.bitb.biz.service.MoneyService;
 
 public class MoneyServiceImpl implements MoneyService {
 
-	private BalanceService balanceService;
+	@Override
+	public boolean depositConfirmRequest(User customer, double money) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean depositConfirm(User customer, double money,
+			boolean isDeposited, User customerService) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean withdrawDepositRequest(User customer, double money) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean withdrawDepositConfirmRequest(User customer, double money,
+			User customerService) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean withdrawDepositConfirm(User customer, double money,
+			boolean isWithdrawed) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 	
-	@Override
-	public boolean withdrawDepositRequest(User user, double money) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean depositConfirmRequest(User user, double money) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean depositConfirm(User user, double money) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean withdrawDepositConfirmRequest(User user, double money) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean withdrawDepositConfirm(User user, double money) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
 }
